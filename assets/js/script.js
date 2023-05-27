32878a09d08ce1dfbf2dcef303d7e573db3a0d2d
abirul-islam-mamun <mamun.billa444@gmail.com>
2023-05-27 17:37:21 +0600

    third running

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,6 +161,72 @@ $('.second-team-active').slick({
 });
 
 
+// Working process  
+$('.working-process-active').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  responsive: [
+    // {
+    //   breakpoint: 1024,
+    //   settings: {
+    //     slidesToShow: 3,
+    //     slidesToScroll: 3,
+    //     infinite: true,
+    //     dots: false
+    //   }
+    // },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+// = POPUP VIDEO
+// -------------------------------------------*/
+if ($(".video-btn").length) {
+    $(".video-btn").on("click", function(){
+        $.fancybox({
+            href: this.href,
+            type: $(this).data("type"),
+            'title'         : this.title,
+            helpers     : {
+                title : { type : 'inside' },
+                media : {}
+            },
+
+            beforeShow : function(){
+                $(".fancybox-wrap").addClass("gallery-fancybox");
+            }
+        });
+        return false
+    });
+}
 
 // odometer 
 if ($(".odometer").length) {
